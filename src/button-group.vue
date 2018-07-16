5b25beac84ccdc30c4a9ab63276b7f5a875e7551
lanlanlanya <282134012@qq.com>
2018-07-16 17:13:56 +0800

    创建 button group

---
+++ src/button-group.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    // export  default {
+    //     mounted(){
+    //         console.log('here');
+    //         console.log(this.children);
+    //     }
+    // }
+</script>
+<style lang="scss">
+    .g-button-group{
+        display:inline-flex;
+        vertical-align: top;
+        >.g-button{
+            border-radius:0;
+          margn-left:-1px;
+            &:first-child{
+            border-top-left-radius:var(--border-radius);
+            border-bottom-left-radius:var(--border-radius);
+        }
+            &:last-child{
+            border-top-right-radius:var(--border-radius);
+            border-bottom-right-radius:var(--border-radius);
+        }
+            &:hover{
+                position:relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
