247d377e51aaf6856ce04537ecb468988f4bb1d3
lanlanlanya <282134012@qq.com>
2018-08-07 11:27:26 +0800

    重构 Row 和 Col

--- src/col.vue
+++ src/col.vue
@@ -1,29 +1,60 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="colClass"  :style="colStyle">
+            <slot></slot>
     </div>
 </template>
 <script>
     export default{
         name:'GuluCol',
+        data(){
+            return{
+                 gutter:0
+            }
+        },
         props:{
             span:{
                 type:[Number,String]
+            },
+            offset:{
+                type:[String,Number]
             }
+        },
+        computed:{
+            colClass(){
+                let {span ,offset}=this;
+                return [
+                    span && `col-${span}`,
+                    offset && `offset-${offset}`
+                ]
+            },
+            colStyle () {
+                return{
+                    paddingLeft:this.gutter/2+'px',
+                    paddingRight:this.gutter/2+'px'
+                }
+            }
+
         }
     }
 </script>
 <style scoped lang="scss">
     .col{
-        height: 100px;
-        background: grey;
+        height: 80px;
+        /*background: grey;*/
         width:50%;
-        border:1px solid red;
+        /*border:1px solid red;*/
+        /*padding:0 10px;*/
         $class-prefix: col-;
         @for $n from 1 through 24 {
             &.#{$class-prefix}#{$n} {
             width: ($n / 24)* 100%;
            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n / 24)* 100%;
+            }
         }
     }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,10 +1,37 @@
 <template>
-    <div class="row">
+    <div class="row" :style="rowStyle">
         <slot></slot>
     </div>
 </template>
+<script>
+    export default{
+        name:'GuluRow',
+        props:{
+            gutter:{
+                type:[Number,String]
+            }
+        },
+        mounted(){
+            this.$children.forEach((vm)=>{
+                console.log(vm);
+                vm.gutter=this.gutter;
+            })
+        },
+        computed:{
+            rowStyle(){
+                let {gutter}=this;
+                return {
+                    marginLeft:-gutter/2+'px',
+                    marginRight:-gutter/2+'px'
+                }
+            }
+        }
+
+    }
+</script>
 <style  scoped lang="scss">
     .row{
         display:flex;
+       /*margin:0 -10px;*/
     }
 </style>
