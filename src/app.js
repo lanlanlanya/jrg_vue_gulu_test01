dc2e7b209a6e45b4b8e3e4a171c0cb83d1318f70
lanlanlanya <282134012@qq.com>
2018-08-10 16:14:50 +0800

    实现了只能有一个toast的功能

--- src/app.js
+++ src/app.js
@@ -40,25 +40,22 @@ new Vue({
         //     inputElement.dispatchEvent(event);
         //     console.log('hi');
         // },3000)
-         this.$toast('你的智商需要充值！',{
-             position:'middle',
-             enableHtml:false,
-             closeButton:{
-                 text:'已充值',
-                 callback(){
-                     console.log('他说已经充值智商了');
-                 }
-             },
-             autoClose:false,
-             autoCloseDelay:3
-         });
+
     },
     methods:{
-        inputChange(e){
-            // console.log(e);
-        },
         showToast(){
-            this.$toast('当前功能不稳定，如果遇到bug请关闭该功能');
+            this.$toast(`你的智商目前为${parseInt(Math.random()*100)},你的智商需要充值！`,{
+                position:'middle',
+                enableHtml:false,
+                closeButton:{
+                    text:'已充值',
+                    callback(){
+                        console.log('他说已经充值智商了');
+                    }
+                },
+                autoClose:false,
+                autoCloseDelay:3
+            });
         }
     }
 });
--- src/plugin.js
+++ src/plugin.js
@@ -1,14 +1,49 @@
 import Toast from './toast'
+let currrentToast;
 export default {
     install(Vue,options){
         Vue.prototype.$toast=function(message,toastOptions){
-            let Constructor=Vue.extend(Toast);
-            let toast=new Constructor({
-                propsData:  toastOptions
-            });
-            toast.$slots.default=[message];
-            toast.$mount();
-            document.body.appendChild(toast.$el);
+            if(currrentToast){
+                currrentToast.close();
+            }
+            currrentToast=createToast({Vue,message,propsData:toastOptions});
         }
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*helpers*/
+function createToast({Vue,message,propsData}){
+    let Constructor=Vue.extend(Toast);
+    let toast=new Constructor({ propsData });
+    toast.$slots.default=[message];
+    toast.$mount();
+    document.body.appendChild(toast.$el);
+    return toast;
 }
