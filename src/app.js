7bc5337d3b5ea2193def67289ae0faa85caf51c4
lanlanlanya <282134012@qq.com>
2018-08-10 15:28:15 +0800

    实现autoClose ,closeButton,enableHtml 和 多行文字等功能

--- src/app.js
+++ src/app.js
@@ -56,7 +56,6 @@ import chai from 'chai'
 import spies from 'chai-spies'
 chai.use(spies);
 const expect=chai.expect;
-
     {
         // console.log(Button);
         const Constructor = Vue.extend(Button);
--- src/toast.vue
+++ src/toast.vue
@@ -2,12 +2,54 @@
 
     <div class="toast">
         <slot></slot>
+        <div class="line"></div>
+        <span class="close" i-if="closeButton" @click="onClickClose">{{closeButton.text}}</span>
     </div>
 </template>
 <script>
-
+// 构造组件的选项
    export  default {
-       name:'GuluToast'
+       name:'GuluToast',
+       props:{
+           autoClose:{
+               type:Boolean,
+               default:true
+           },
+           autoCloseDelay:{
+               type:Number,
+               default:50
+           },
+           closeButton:{
+               type:Object,
+               default(){
+                   return {
+                       text:'关闭',callback:(toast)=>{
+                           toast.close();
+                       }
+                   }
+               }
+           }
+       },
+       created(){
+           console.log(this.closeButton);
+       },
+       mounted(){
+           if (this.autoClose){
+               setTimeout(()=>{
+                   this.close();
+                   },this.autoCloseDelay*1000);
+           }
+       },
+       methods:{
+           close(){
+               this.$el.remove();
+               this.$destroy();
+           },
+           onClickClose(){
+               this.close();
+               this.closeButton.callback();
+           }
+       }
    }
 
 </script>
@@ -30,5 +72,12 @@
         border-radius:4px;
         box-shadow:0 0 3px 0 rgba(0,0,0,0.50);
         padding:0 16px;
+    }
+    .close{
+        padding-left:16px;
+    }
+    .line{
+        height:100%;
+        border:1px solid #222;
     }
 </style>
