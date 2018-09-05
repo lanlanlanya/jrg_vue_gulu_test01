a2445d948faab1729ad5415c67d0a9654cf9e6b4
lanlanlanya <282134012@qq.com>
2018-09-05 14:13:54 +0800

    popover 功能完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,12 +1,11 @@
 <template>
-   <div class="popover" @click.stop="xxx">
+   <div class="popover" @click="onClick" ref="popover">
        <div ref="contentWrapper" class="content-wrapper"  v-if="visible" @click.stop>
            <slot name="content"></slot>
        </div>
        <span ref="triggerWrapper">
             <slot ></slot>
        </span>
-
    </div>
 </template>
 
@@ -19,25 +18,39 @@
             }
         },
         methods:{
-            xxx(){
-               this.visible=!this.visible;
-               if(this.visible===true){
-                   this.$nextTick(()=> {
-                       document.body.appendChild(this.$refs.contentWrapper);
-                      let {width,height,top,left}= this.$refs.triggerWrapper.getBoundingClientRect();
-                      this.$refs.contentWrapper.style.left=left+'px';
-                      this.$refs.contentWrapper.style.top=top+'px';
-                       let eventHandler = () => {
-                           this.visible = false;
-                           document.removeEventListener('click', eventHandler);
-                           // console.log('document隐藏popover');
-                       }
-                       document.addEventListener('click', eventHandler);
-                   });
-               }else{
-                   // console.log('vm隐藏popover');
-               }
+            onClickDocument (e) {
+                if (this.$refs.popover &&
+                    (this.$refs.popover===e.target || this.$refs.popover.contains(e.target))
+                ){  return; }
+                this.close();
+            },
+            positionContent(){
+                document.body.appendChild(this.$refs.contentWrapper);
+                let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
+                this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+            },
+            open(){
+                this.visible=true;
+                this.$nextTick(() => {
+                    this.positionContent();
+                    document.addEventListener('click', this.onClickDocument);
+                });
+            },
+            close(){
+                this.visible=false;
+                document.removeEventListener('click', this.onClickDocument);
+            },
+            onClick(event) {
+                if (this.$refs.triggerWrapper.contains(event.target)) {
+                    if (this.visible === true) {
+                        this.close();
+                    }else{
+                        this.open();
+                    }
+                }
             }
+
         },
         mounted(){
 
