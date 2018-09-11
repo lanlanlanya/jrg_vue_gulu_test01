03a5cf439e328b2c88190329d742a97428ae1c0a
lanlanlanya <282134012@qq.com>
2018-09-11 16:41:46 +0800

    selected 改为数组

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ new Vue({
         loading2:true,
         loading3:false,
         message:"hello",
-        selectedTab:'2'
+        selectedTab:['2','1']
     },
     created(){
         // setTimeout(()=>{
