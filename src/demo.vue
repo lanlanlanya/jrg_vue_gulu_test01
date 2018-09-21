a7fe21c0e919d81214eaafcb6101a7ec6b830084
lanlanlanya <282134012@qq.com>
2018-09-21 15:34:15 +0800

    导入数据库

--- src/demo.vue
+++ src/demo.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <div style="padding:20px;">
             <g-cascader :source="source" popover-height="200px"
-                        :selected="selected" @update:selected="selected=$event"></g-cascader>
+                        :selected.sync="selected"></g-cascader>
             <g-button>你好</g-button>
         </div>
     </div>
@@ -11,30 +11,18 @@
 <script>
     import Button from './button'
     import Cascader from './cascader'
+    import db from './db'
+    function ajax(parentId=0){
+            return db.filter((item)=>item.parent_id===parentId);
+    }
+
+    console.log(ajax());
     export default {
         name: "demo",
         data(){
             return {
                 selected:[],
-                source:[
-                    {name:'浙江',
-                        children:[
-                            {name: "杭州", children: [{ name: "上城区"},{name: "下城区"}, {name: "江干区"}]},
-                            {name: "嘉兴", children: [{ name: "南湖区"},{name: "秀洲区"}, {name: "嘉善区"}]}
-                        ]
-                    },
-                    {name:'福建',
-                        children:[
-                            {name: "福州", children: [{ name: "鼓楼区"},{name: "台江区"}, {name: "仓山区"}]}
-                        ]
-                    },
-                    {name:'河北',
-                        children:[
-                            {name: "石家庄", children: [{ name: "桥东区"},{name: "桥西区"}, {name: "开发区"}]},
-                            {name: "邯郸市", children: [{ name: "磁县"},{name: "涉县"}, {name: "大名"}]}
-                        ]
-                    }
-                ]
+                source:ajax()
             }
         },
         components:{
