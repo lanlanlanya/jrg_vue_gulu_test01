c5e8c68e01105cec7d42cceee066303c8ae4086c
lanlanlanya <282134012@qq.com>
2018-10-29 16:56:07 +0800

    nav基本结构完成

---
+++ src/nav/sub-nav.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="g-sub-vue">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GuluSubNav"
+    }
+</script>
+
+<style scoped lang="scss">
+.g-sub-vue{
+
+}
+</style>
